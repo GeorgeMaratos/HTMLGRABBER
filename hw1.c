eea1271541c3298e8603d9dedc1f6b7db1a30e7b
George Maratos <gmaratos@comcast.net>
2015-01-19 00:08:03 -0600

    added refrence list

--- hw1.c
+++ hw1.c
@@ -2,6 +2,10 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+/* refrences
+http://www.amiga.org/forums/archive/index.php/t-20341.html
+http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html#connect
+*/
 void 
 main(int argc, char **argv)
 {
